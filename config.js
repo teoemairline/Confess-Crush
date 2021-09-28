2e6e06b1ce365e613cf25e50ecdd2059223f1790
teoemairline <91327346+teoemairline@users.noreply.github.com>
2021-09-28 10:40:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Tèo <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Tèo biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, Tèo qua đón em đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=3MKq8tfLiMY' //link mess của các bạn. VD: https://m.me/nam.nodemy
