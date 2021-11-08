9f44ed4dce6c9c2af22532447ecd769b48cdd5b7
teoemairline <91327346+teoemairline@users.noreply.github.com>
2021-11-08 22:36:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Test",
-    introTitle: 'Thanh Tâm à',
+    introTitle: 'BlueShark à',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu anh Tèo ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say anh Tèo ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích anh Tèo lắm <33',
+    btnIntro: '^^Xin chào^^',
+    title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say anh ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích anh lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Tèo <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Tèo biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, Tèo qua đón em đi chơi nha.',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, Anh ra Vũng Tàu đón em đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=3MKq8tfLiMY' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
